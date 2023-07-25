8deb9f08d43f6ac312e8d3de6b0337d9ff52b941
slickmoto <35445830+slickmoto@users.noreply.github.com>
2023-07-24 20:19:56 -0500

    Update and rename redirect.php to redirect.js

---
+++ pages/redirect.js
@@ -0,0 +1,60 @@
+import { useEffect } from 'react';
+
+const RedirectPage = () => {
+  useEffect(() => {
+    // Add your Bitly client ID and secret here
+    const clientID = 'YOUR_BITLY_CLIENT_ID';
+    const clientSecret = 'YOUR_BITLY_CLIENT_SECRET';
+
+    // Get the query parameter "code" from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const authCode = urlParams.get('code');
+
+    // Function to make a POST request to Bitly API
+    const callBitlyAPI = async () => {
+      const accessTokenURL = 'https://api-ssl.bitly.com/oauth/access_token';
+      const accessTokenData = {
+        client_id: clientID,
+        client_secret: clientSecret,
+        code: authCode,
+      };
+
+      try {
+        const response = await fetch(accessTokenURL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(accessTokenData),
+        });
+
+        const data = await response.json();
+        const accessToken = data.access_token;
+
+        // Save the access token to use in your extension
+        // You can store it in localStorage or cookies as needed
+        // For this example, we'll just log it for demonstration purposes
+        console.log('Access Token:', accessToken);
+
+        // Replace 'YOUR_EXTENSION_URL' with the actual URL of your Etsy URL Shortener Extension
+        window.location.replace('YOUR_EXTENSION_URL');
+      } catch (error) {
+        console.error('Error:', error);
+        // Redirect the user back to your extension with an error message
+        window.location.replace('YOUR_EXTENSION_URL?error=1');
+      }
+    };
+
+    if (authCode) {
+      // Call the Bitly API to exchange the authorization code for an access token
+      callBitlyAPI();
+    } else {
+      // Redirect the user back to your extension with an error message if no code is provided
+      window.location.replace('YOUR_EXTENSION_URL?error=1');
+    }
+  }, []);
+
+  return null;
+};
+
+export default RedirectPage;
