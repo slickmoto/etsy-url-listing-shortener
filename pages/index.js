cb9a392e77d3a99434113156aa32c6f718237838
slickmoto <35445830+slickmoto@users.noreply.github.com>
2023-07-25 18:59:08 -0500

    Update index.js
    Added css to give a dark terminal look.

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,37 @@ export default function LandingPage() {
         <meta name="description" content="Etsy URL Shortener Extension Landing Page" />
       </Head>
       <main>
-        <h1>Welcome to the Etsy URL Shortener Extension</h1>
-        <p>
-          This landing page provides information about the Etsy URL Shortener Firefox Extension.
-        </p>
-        <p>Download the extension from the Firefox Add-ons store.</p>
+        <style jsx>{`
+          body {
+            background-color: #121212;
+            color: #f58020;
+            font-family: 'Courier New', monospace;
+            margin: 0;
+            padding: 0;
+          }
+
+          .container {
+            text-align: center;
+            padding: 20px;
+          }
+
+          h1 {
+            font-size: 24px;
+            margin-bottom: 20px;
+          }
+
+          p {
+            font-size: 16px;
+            margin-bottom: 10px;
+          }
+        `}</style>
+        <div className="container">
+          <h1>Welcome to the Etsy URL Shortener Extension</h1>
+          <p>
+            This landing page provides information about the Etsy URL Shortener Firefox Extension.
+          </p>
+          <p>Download the extension from the Firefox Add-ons store.</p>
+        </div>
       </main>
     </div>
   );
